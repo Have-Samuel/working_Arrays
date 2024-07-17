ea1074de9616ba1a9025f89592830eaad1e52f24
samuel <samiehave9@gmail.com>
2024-07-17 16:02:10 +0300

    Created the checking_out file and doing the Arrays challenges

--- checking_out.rb
+++ checking_out.rb
@@ -1,4 +1,21 @@
-num = [1, 3, 5, 7, 9, 11]
+arr = [1, 3, 5, 7, 9, 11]
 number = 3
-puts num.include?(number) # => true
+puts arr.include?(number) # => true
  
+arr.each do |num|
+    if num == number
+        puts "#{number} is inthe array."
+    end
+end
+
+if arr.include?(number)
+    puts "#{number} is in the array."
+end
+
+arr = ["b", "a"]
+arr = arr.product(Array(1..3))
+arr.first.delete(arr.first.last)
+
+arr = ["b", "a"]
+   arr = arr.product([Array(1..3)])
+   arr.first.delete(arr.first.last)
