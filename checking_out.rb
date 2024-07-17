3709669d71d3b22e6abf1d189f9da8930416f78e
samuel <samiehave9@gmail.com>
2024-07-17 16:30:43 +0300

    Challenge2

--- checking_out.rb
+++ checking_out.rb
@@ -12,10 +12,17 @@ if arr.include?(number)
     puts "#{number} is in the array."
 end
 
-arr = ["b", "a"]
-arr = arr.product(Array(1..3))
-arr.first.delete(arr.first.last)
+arr1 = ["b", "a"]
+arr1 = arr1.product(Array(1..3))
+arr1.first.delete(arr1.first.last)
 
-arr = ["b", "a"]
-   arr = arr.product([Array(1..3)])
-   arr.first.delete(arr.first.last)
+puts arr1
+p arr1
+print arr1
+arr2 = ["x","y"]
+   arr2 = arr2.product([Array(1..3)])
+   arr2.first.delete(arr2.first.last)
+
+puts arr2
+p arr2
+print arr2
